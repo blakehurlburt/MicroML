6a2729ddf4b5738af9b99afccde44ab6b9456aab
Grant Iraci <grantira@buffalo.edu>
2016-11-06 11:58:51 -0500

    Final Commit

--- arduino_bindings.h
+++ arduino_bindings.h
@@ -5,62 +5,68 @@
 #include "runtime.h"
 
 void println__(Obj * o, Environment * env) {
-  if(o->type() == "int")
-    Serial.println(unwrapInt(o));
+    if(o->type() == "int") {
+        Serial.println(unwrapInt(o));
+    } else if(o->type() == "real") {
+        Serial.println(unwrapReal(o));
+    } else if(o->type() == "record") {
+        Record* rec = (Record*) o;
+        if(rec->r.empty() || rec->r.count("head") <= 0) {
+            return;
+        }
 
-  else if (o->type() == "real")
-    Serial.println(unwrapReal(o));
-
-  else if (o->type() == "record") {
-    Record* rec = (Record*) o;
-    if (rec->r.empty() || rec->r.count("head") <= 0) return;
-
-    if (rec->get("head").type() == "char")
-      Serial.println(unwrapString(o));
-    else
-      Serial.println(unwrapList(o));
+        if(rec->get("head").type() == "char") {
+            Serial.println(unwrapString(o));
+        } else {
+            Serial.println(unwrapList(o));
+        }
     }
-  }
+}
 
 }
 
 void pinMode__(Obj * o, Environment* env) {
-  if (o->type() != "record")
-    _error_("not a record");
+    if(o->type() != "record") {
+        _error_("not a record");
+    }
 
-  Rec* rec = (Rec*) o;
-  Object* pin = rec->get("pin"),
+    Rec* rec = (Rec*) o;
+    Object* pin = rec->get("pin"),
           val = rec->get("mode");
 
-  if (pin->type() != "integer" || mode->type() != "integer")
-    _error_("arguments are not ints");
+    if(pin->type() != "integer" || mode->type() != "integer") {
+        _error_("arguments are not ints");
+    }
 
-  pinMode(unwrapInt(pin), unwrapInt(mode));
+    pinMode(unwrapInt(pin), unwrapInt(mode));
 }
 
 void digitalWrite__(Obj * o, Environment* env) {
-  if (o->type() != "record")
-    _error_("not a record");
+    if(o->type() != "record") {
+        _error_("not a record");
+    }
 
-  Record* rec = (Record*) o;
-  Object* pin = rec->get("pin"),
+    Record* rec = (Record*) o;
+    Object* pin = rec->get("pin"),
           val = rec->get("val");
 
-  if (pin->type() != "integer" || val->type() != "integer")
-    _error_("arguments are not ints");
+    if(pin->type() != "integer" || val->type() != "integer") {
+        _error_("arguments are not ints");
+    }
 
-  digitalWrite(unwrapInt(pin), unwrapInt(val));
+    digitalWrite(unwrapInt(pin), unwrapInt(val));
 }
 
 Char* read__() {
-  return makeChar(Serial.read());
+    return makeChar(Serial.read());
 }
 
 
 void delay__(Object * o, Environment * env) {
-  if (o.type() != "integer")
-    __error__("not an integer");
-  delay(unwrapInt(o));
+    if(o.type() != "integer") {
+        __error__("not an integer");
+    }
+    delay(unwrapInt(o));
 }
 
 #endif
